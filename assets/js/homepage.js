7b8aff112dbee441e21af201d0fd40a7dac10cb6
Hamzah <hamzahullah@yahoo.com>
2021-05-27 13:43:48 -0400

    Finished searching for featured languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,20 @@
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
+
 var getUserRepos = function (user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -49,14 +63,14 @@ var displayRepos = function (repos, searchTerm) {
     repoSearchTerm.textContent = searchTerm;
     // loop over repos
     for (var i = 0; i < repos.length; i++) {
-  // format repo name
-  var repoName = repos[i].owner.login + "/" + repos[i].name;
- 
-  // create a container for each repo
-  var repoEl = document.createElement("a");
-  repoEl.classList = "list-item flex-row justify-space-between align-center";
-  repoEl.setAttribute("href", "./single-repo.html?repo=" +repoName);
-  // create a span element to hold repository name
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("a");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+        // create a span element to hold repository name
         var titleEl = document.createElement("span");
         titleEl.textContent = repoName;
 
@@ -82,7 +96,22 @@ var displayRepos = function (repos, searchTerm) {
         repoEl.appendChild(statusEl);
     }
 };
+var buttonClickHandler = function (event) {
+    // get the language attribute from the clicked element
+    var language = event.target.getAttribute("data-language");
+
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+};
+
+
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
